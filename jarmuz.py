a09dbd9a1e4ca8b5ecf95d267bc39b9358ec31d0
ccoverstreet <coverst2@vols.utk.edu>
2020-03-28 23:04:42 -0400

    Added helper functions

--- jarmuz.py
+++ jarmuz.py
@@ -16,6 +16,15 @@ jarmuz_dir = os.path.abspath(__file__).replace("/jarmuz.py", "")
 
 cwd = os.getcwd()
 
+def jarmuz_info():
+    print(
+'''Jarmuz Command Line Tool
+Cale Overstreet
+
+Usage:
+    install packageauthor/packagename:\tInstalls selected package from github if repository has jarmuzconfig.json
+    remove packageauthor/packagename:\tRemoves selected pacakge from local installation''')
+
 def jarmuz_install(arguments):
     for i in range(0, len(arguments)):
         if len(arguments[i].split("/")) != 2:
@@ -79,20 +88,41 @@ def jarmuz_start(arguments):
         except:
             print("FATAL: Unable to start package")
 
+def jarmuz_programs():
+    # Lists all installed programs
+    authors = os.listdir(jarmuz_dir + "/sources")
+    authors.remove("SOURCES_README.txt") # Remove extra file
 
+    # Iterate through all authors and repos to print program names
+    for name in authors:
+        repo_names = os.listdir(jarmuz_dir + "/sources/" + name)
+        for repo in repo_names:
+            print(f"{name}/{repo}")
+    
 
 
 def main():
     # Main execution sequence tht determines what helper functions are needed
 
+    if len(sys.argv) == 1:
+        jarmuz_info()
+
     # Loop through all arguments
     for i in range(1, len(sys.argv)):
         if sys.argv[i] == "install":
             jarmuz_install(sys.argv[i + 1:len(sys.argv)]) # Sends remaining argumens to helper function
+            return
         elif sys.argv[i] == "remove":
             jarmuz_remove(sys.argv[i + 1: len(sys.argv)])
+            return
         elif sys.argv[i] == "start":
             jarmuz_start(sys.argv[i + 1: len(sys.argv)])
+            return
+        elif sys.argv[i] == "programs":
+            jarmuz_programs()
+            return
+        else:
+            jarmuz_info()
 
 
 if __name__ == "__main__":
